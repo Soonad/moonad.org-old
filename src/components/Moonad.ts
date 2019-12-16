46506621b29bd45537906b4dc214bed5130f00db
Maisa <maisa.milena@gmail.com>
2019-12-16 09:47:21 -0300

    Add test for Local Files

--- src/components/Moonad.ts
+++ src/components/Moonad.ts
@@ -93,22 +93,23 @@ const get_local_files = () => {
   return window.localStorage.getItem("saved_local");
 }
 
-const save_local_file = (file: LocalFile) => {
+const save_local_file = (file: LocalFile, input_name?: () => string) => {
 
   if(file){
     const local_files = get_local_files();
     const load_result: LocalFile | null = load_local_file(file.file_name);
-    
     if (!local_files) {
-      const name = prompt("Please enter the file name", "");
-      name ? file.file_name = name : file.file_name = "empty_name";
+      if(input_name){
+        file.file_name = input_name();
+      }
       window.localStorage.setItem("saved_local", JSON.stringify([file]));
     } else { 
       const new_files: LocalFile[] = JSON.parse(local_files);
       window.localStorage.removeItem("saved_local");
       if (!load_result){ // There's not file with this name
-        const name = prompt("Please enter the file name", "");
-        name ? file.file_name = name : file.file_name = "empty_name";
+        if(input_name){
+          file.file_name = input_name();
+        }
         new_files.push(file);
       } else { // File exists
         // Find and update file
@@ -373,7 +374,15 @@ class Moonad extends Component {
     // Only saves a file in editing mode
     if(this.mode === "EDIT"){
       const save: LocalFile = {code: this.code, file_name: this.file};
-      const saved_file = save_local_file(save);
+      const get_name = () => {
+        const name = prompt("Please enter the file name", "");
+        if(name) {
+          return name;
+        } else {
+          return "empty_string";
+        }
+      }
+      const saved_file = save_local_file(save, get_name);
       if(saved_file !== false) {
         this.code = saved_file.code;
         this.file = saved_file.file_name;
@@ -447,4 +456,5 @@ class Moonad extends Component {
 }
 
 export {Moonad, load_file, parse_file, load_file_parents, type_check_term, 
-  reduce, BaseAppPath }
+  reduce, BaseAppPath,
+  save_local_file, load_local_file, get_local_files, delete_local_file }
--- src/tests/LocalFiles.test.ts
+++ src/tests/LocalFiles.test.ts
@@ -1,23 +1,23 @@
 
-import {load_local_file, save_local_file, get_local_files} from "../components/Moonad"
+import {delete_local_file, get_local_files, load_local_file, save_local_file } from "../components/Moonad"
 import {LocalFile} from "../assets/Constants";
 
 
 
 describe("Local files", () => {
 
-  // var prompt = jest.fn();
-  // Object.defineProperty(window, 'prompt', prompt);
-
   const globalAny:any = global;
   globalAny.prompt = jest.fn();
 
   beforeAll(() => {
     Object.defineProperty(window, 'prompt', {value: "teste"});
+  })
+
+  beforeAll(() => {
     const item0: LocalFile = {code: "import Base# main 'Hello, World!'", file_name: "item0"};
-    // const res = save_local_file(item0);
-    window.localStorage.setItem("saved_local", JSON.stringify([item0]));
-    // console.log(res);
+    const item1: LocalFile = {code: "import Base# main 'Hello, World!'", file_name: "item1"};
+    const new_files: LocalFile[] = [item0, item1];
+    window.localStorage.setItem("saved_local", JSON.stringify(new_files));
   })
 
   test("Can load a local file if it exists", () => {
@@ -31,19 +31,39 @@ describe("Local files", () => {
   });
 
   test("Can get all local files", () => {
-    const item0: LocalFile = {code: "import Base# main 'Hello, World!'", file_name: "item0"};
-    const item1: LocalFile = {code: "import Base# main 'Hello, World!'", file_name: "item1"};
-    const new_files: LocalFile[] = [item0, item1];
-    window.localStorage.setItem("saved_local", JSON.stringify(new_files));
+    let res = get_local_files();
+    if (res) {
+      let aux = JSON.parse(res);
+      expect(aux).toEqual([{code: "import Base# main 'Hello, World!'", file_name: "item0"},
+      {code: "import Base# main 'Hello, World!'", file_name: "item1"}]);
+    }
+    expect(res).not.toBeNull();
+  });
 
-    const res = get_local_files();
-    expect(res).toBe([{code: "import Base# main 'Hello, World!'", file_name: "item0"},
-    {code: "import Base# main 'Hello, World!'", file_name: "item1"} ]);
+  test("Can delete a local file", () => {
+    let res = delete_local_file("item0");
+    expect(res).toBeTruthy();
+  });
 
-  })
+  test("Cannot delete a local file that don't exist", () => {
+    let res = delete_local_file("itemmm");
+    expect(res).toBeFalsy();
+  });
 
-  // test("It should create a new array if item doesn't exist", () => {
+  test("Can save local file", () => {
+    const item2: LocalFile = {code: "import Base# main 'Hello, World!'", file_name: "item2"};
+    const input = () => {return "item2"}; // simulate an input function
+    const res = save_local_file(item2, input);
 
-  // }, 5000);
+    expect(res).toEqual(item2);
+  });
+
+  test("Can update an existing file", () => {
+    const item1: LocalFile = {code: "import Base# main 'Greetings, Starfleet! I'm commander Riker, the Number One'", file_name: "item1"};
+    const res = save_local_file(item1);
+
+    expect(res).toEqual(item1);
+  });
+
 
 })
